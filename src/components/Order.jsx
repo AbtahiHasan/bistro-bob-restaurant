d7c224f46a11d7f8d35af1b82ac5572c57a6c523
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-22 14:11:57 +0600

    order section basic done

---
+++ src/components/Order.jsx
@@ -0,0 +1,13 @@
+
+const Order = () => {
+    return (
+        <section className="mt-[140px]">
+            <h2 className="text-[#D99904] text-center my-3">---From 11:00am to 10:00pm---</h2>
+            <div className="flex justify-center">
+                <h3 className="border-y-4 inline-block py-2 text-[40px]">ORDER ONLINE</h3>
+            </div>
+        </section>
+    );
+};
+
+export default Order;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../components/Banner";
+import Order from "../components/Order";
 
 
 const Home = () => {
     return (
         <main>
             <Banner/>
+            <Order/>
         </main>
     );
 };
